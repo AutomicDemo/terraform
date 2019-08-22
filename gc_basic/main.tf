953a584f2e887dde3ff0955783cbb654c1feedcd
AutomicDemo <36577894+AutomicDemo@users.noreply.github.com>
2019-08-22 16:48:37 -0400

    Update main.tf

--- gc_basic/main.tf
+++ gc_basic/main.tf
@@ -6,6 +6,7 @@ variable "infrastructure_name" {default = "dev"}
 variable "credentials" {}
 variable "zone" {default = "us-east1-c"}
 
+
 variable "num_nodes" {
   description = "Number of nodes to create"
   default     = 1
@@ -32,6 +33,10 @@ resource "google_compute_instance" "default" {
   zone         = "${var.zone}"
   name         = "${var.infrastructure_name}-${count.index + 1}-${local.id}"
   machine_type = "f1-micro"
+
+  metadata {
+    ssh-keys = "mike:ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAiKq6YB5jGX+rhbBkZF6Z2G+AEf5f6eedwHulIrvRwlkzjo+jD3WmZOffKG9wnh13AoNegRwHuliIdJGVlyCSr7Kse/xj7lwOOle4kHiqv7QlYKr1EkHJIxffTqw8jqBQ5RMRNQFxVliGuvNx0g2p8rf5pkU+1G3lDWkkNjFlbi2A+WhlELoQ3uNGkR6SWtgqdpEhB6d8pelBFWFFEvVsls/PpCnxZORIIPpcQ1pyXvP9xALLJXRdQTOG1DY6dcl766S4Nn/MWZkhh+q9mbsvGl8P5pzuWKMmWl+mySR9nH7q5SxyPtnlHWnRaQ/w8Pa9o0tTDqD4k9jR5loJvRPL+Q== mike"
+  }	
   
   boot_disk {
     initialize_params {
