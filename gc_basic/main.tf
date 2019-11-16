fe719d0eae219391b53a3b3f4ac75f46dd61b2ed
AutomicDemo <36577894+AutomicDemo@users.noreply.github.com>
2019-11-15 22:01:24 -0800

    Created Dummy Values for ssh

--- gc_basic/main.tf
+++ gc_basic/main.tf
@@ -5,8 +5,11 @@ variable "image" {default = "projects/demos-esd-automation/global/images/centos-
 variable "infrastructure_name" {default = "demo"}
 variable "credentials" {}
 variable "zone" {default = "us-east1-c"}
+variable "machine_type" {default = "f1-micro"}
 variable "override" {}
 variable "email" {default = "1088214523720-compute@developer.gserviceaccount.com"}
+variable "ssh_user" {default = "automic"}
+variable "public_key" {default = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAgF8m+KDKerDYe0xoj9042H8DqM7T1Qil0+QxUXTcxLpO/in3wqpphlyVsuGRjFvkeHVCezimMtk6YWW2e2gNv7ugKfj1AoBhwxkcvcEQtCf67RFlGBYMZlzLOXItvAaU2c8eqnTn5lvY14vosNkj4ACVXZJDhNWs3qLLSrEuN3xUsuDgQVE4aNiwfTKYAVahSCFBueL26Zq4tcZaZmfC23YRGOaCvjFeShKqHcQl1nihcHB421oHOGRaZIc4Fwzf6TsRF9+OAA0MLEKIm17VtF6pfxt5rn3sxeVhfBulScH2uhApoB52yY1eDYCQQXmV5uYg8DoTNTFemGQMDHt/2Q== automic"
 
 
 variable "num_nodes" {
@@ -34,8 +37,10 @@ resource "google_compute_instance" "default" {
   project      = "${var.project}"
   zone         = "${var.zone}"
   name         = "${var.override}"
-  machine_type = "f1-micro"
-
+  machine_type = "${var.machine_type}"
+  metadata = {
+    sshKeys = "${var.ssh_user}:${(var.public_key)}"
+  }
   boot_disk {
     initialize_params {
       image = "${var.image}"
